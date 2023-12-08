11cbfc347ed4e33f651411e8e950fffa32c47034
Prince Changani <princechangani56385638@gmail.com>
2023-12-08 17:24:23 +0530

    5) Getting value from the user and displaying on the screen
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Angular';
   name = "Prince Changani";
+  user_name = "";
   getName(firstName:string, lastName:string)
   {
     alert("full name is: "+ firstName + " " + lastName);
+  }
+  getValue(value:string)
+  {
+    console.warn(value);
+  }
+  getUser(value:string)
+  {
+    console.log("The user name is: " + value);
+    this.user_name = value;
   }
 }
