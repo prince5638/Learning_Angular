e3f175ff8ed58a7083570de064af0d4cf6c2ae3a
Prince Changani <princechangani56385638@gmail.com>
2023-12-08 09:48:24 +0530

    1) Created one component and added some style to app component
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular Blog';
+  title = 'Angular';
   name = "Prince Changani";
 }
