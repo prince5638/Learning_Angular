ebc81b0cd59294d7ebcadc97f21106fc0d0c0761
Prince Changani <princechangani56385638@gmail.com>
2023-12-09 16:40:17 +0530

    13) BAsic Form in angular
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- src/app/user-auth/login/login.component.ts
+++ src/app/user-auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  userData:any = {};
+  getData(data:NgForm)
+  {
+    console.warn(data)
+    this.userData = data;
+  }
 }
--- src/app/user-auth/user-auth.module.ts
+++ src/app/user-auth/user-auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -9,10 +10,12 @@ import { LoginComponent } from './login/login.component';
     LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     LoginComponent
   ]
 })
+
 export class UserAuthModule { }
